2e099ba6a93a3b46686f64843af4a328f25b8ac0
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-09 14:08:34 +0300

    chore(deps): update flake deps

--- overlays/hydrus.nix
+++ overlays/hydrus.nix
@@ -1,12 +1,16 @@
 final: prev: {
   hydrus = prev.hydrus.overrideAttrs (old: rec {
     pname = "hydrus";
-    version = "433";
-    src = prev.fetchFromGitHub {
-      owner = "hydrusnetwork";
-      repo = "hydrus";
-      rev = "v433";
-      sha256 = "sha256-RZKmtVSCkMuJxuGGgk92J0Y71aHRZYsaBmUZy/gC9Ms=";
+    version = "434";
+    src = builtins.fetchGit {
+      url = "https://github.com/hydrusnetwork/hydrus.git";
+      rev = "71bedccaeabf411307edeac3b03a0903d2c23ec8";
     };
+    postPatch = ''
+      sed 's;os\.path\.join(\sHC\.BIN_DIR,.*;"${prev.miniupnpc_2}/bin/upnpc";' \
+        -i ./hydrus/core/networking/HydrusNATPunch.py
+      sed 's;os\.path\.join(\sHC\.BIN_DIR,.*;"${prev.swftools}/bin/swfrender";' \
+        -i ./hydrus/core/HydrusFlashHandling.py
+    '';
   });
 }
