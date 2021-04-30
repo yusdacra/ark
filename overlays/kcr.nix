ab8597812c6af9080b118c13522664aa8044d61c
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-04-30 05:26:24 +0300

    chore(deps): update flake deps

--- overlays/kcr.nix
+++ overlays/kcr.nix
@@ -6,7 +6,7 @@ final: prev: {
     src = prev.fetchzip {
       url = "https://github.com/alexherbo2/kakoune.cr/releases/download/nightly/kakoune.cr-nightly-x86_64-unknown-linux-musl.zip";
       stripRoot = false;
-      sha256 = "sha256-3OqhuSXKZE/gakbUN/0iSqcsjvQnnXBcVq/zc7vMO04=";
+      sha256 = "sha256-Ly6zsm2cea1GUpgwmmlkDO2UI0wsp/tSQSZpfvjLCrM=";
     };
 
     installPhase = ''
