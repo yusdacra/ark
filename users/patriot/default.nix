a28b2705519cd554aabebf0880134445354fc59e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-05-03 07:54:46 +0300

    fix: starship symbols

--- users/patriot/default.nix
+++ users/patriot/default.nix
@@ -635,7 +635,10 @@ in
           enable = true;
           settings = {
             add_newline = false;
-            character.symbol = ">";
+            character = {
+              success_symbol = "[>](bold green)";
+              error_symbol = "[>](bold red)";
+            };
             directory = {
               truncation_length = 2;
               truncate_to_repo = false;
