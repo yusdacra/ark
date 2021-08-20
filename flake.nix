08c79b45fb5ba9b31adbe21205640b969a7125a9
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-08-20 16:47:30 +0300

    stuf

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs =
     {
-      nixos.url = "github:nixos/nixpkgs/release-21.05";
+      nixos.url = "github:nixos/nixpkgs/nixos-21.05";
       latest.url = "github:nixos/nixpkgs/nixos-unstable";
 
       digga.url = "github:divnix/digga/main";
