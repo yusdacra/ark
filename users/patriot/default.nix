92c7c9926440949fa188be3b95fab26257e312c5
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-04-10 14:07:13 +0300

    better helix conf

--- users/patriot/default.nix
+++ users/patriot/default.nix
@@ -693,8 +693,25 @@ in {
         '';
         "helix/config.toml".text = ''
           theme = "mytheme"
+
           [editor]
           line-number = "relative"
+          middle-click-paste = false
+          true-color = true
+
+          [editor.cursor-shape]
+          insert = "bar"
+
+          [editor.auto-pairs]
+          '(' = ')'
+          '{' = '}'
+          '[' = ']'
+          '"' = '"'
+          "'" = "'"
+          '<' = '>'
+
+          [editor.lsp]
+          display-messages = true
         '';
         "helix/languages.toml".text = ''
           [[language]]
