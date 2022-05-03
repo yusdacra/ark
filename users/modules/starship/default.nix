21be00c4cfbe8acfe113f36e1d28457c47f7a9f5
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-05-03 23:34:41 +0300

    hhh

--- users/modules/starship/default.nix
+++ users/modules/starship/default.nix
@@ -2,15 +2,19 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "$shell$nix_shell$shlvl@ $directory> ";
+      format = "$shell$shlvl$nix_shell at $directory> ";
       add_newline = false;
       character = {
         success_symbol = "[>](bold green)";
         error_symbol = "[>](bold red)";
       };
       shell.disabled = false;
-      shlvl.disabled = false;
-      nix_shell.format = "via [$name]($style) ";
+      shlvl = {
+        disabled = false;
+        symbol = "shlvl ";
+        format = "on [$symbol$shlvl]($style) ";
+      };
+      nix_shell.format = "via [$name]($style)";
       directory = {
         truncation_length = 2;
         truncate_to_repo = false;
