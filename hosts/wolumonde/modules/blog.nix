34e24a829d3bf0596d0abffe49d9b96500b9a701
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-08-14 10:07:26 +0300

    fixes

--- hosts/wolumonde/modules/blog.nix
+++ hosts/wolumonde/modules/blog.nix
@@ -6,7 +6,7 @@
   services.nginx.virtualHosts."gaze.systems" = {
     enableACME = true;
     forceSSL = true;
-    root = "${inputs.blog.packages.${pkgs.system}.website}";
+    root = "${inputs.blog.packages.${pkgs.system}.blog}";
     locations."/".extraConfig = ''
       add_header cache-control max-age=1800;
     '';
