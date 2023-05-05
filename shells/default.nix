cef3293400f38193334b657105bda5b4b88e7baf
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-05-05 03:09:46 +0300

    wip

--- shells/default.nix
+++ shells/default.nix
@@ -1,9 +1,23 @@
 {tlib, ...}:
-tlib.genPkgs (pkgs: {
+tlib.genPkgs (pkgs: let
+  agenix-wrapped = pkgs.writeShellApplication {
+    name = "agenix";
+    runtimeInputs = [pkgs.agenix];
+    text = ''
+      if [ -z "''${1-}" ]; then
+        agenix
+      else
+        RULES="/etc/nixos/secrets/secrets.nix" agenix -i /etc/nixos/keys/ssh_key "$@"
+      fi
+    '';
+  };
+in {
   default = with pkgs;
     mkShell {
       name = "prts";
-      buildInputs = [git git-crypt alejandra helix agenix];
-      shellHook = "echo \"$(tput bold)welcome to PRTS, $USER$(tput sgr0)\"";
+      buildInputs = [git git-crypt alejandra helix agenix-wrapped rage];
+      shellHook = ''
+        echo \"$(tput bold)welcome to PRTS, $USER$(tput sgr0)\"
+      '';
     };
 })
