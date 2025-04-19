9950c46fdfe8bf0a9cf0731ff2f1f18ee5309468
dusk <y.bera003.06@protonmail.com>
2025-04-19 21:43:05 +0900

    feat: disable wg

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,8 +2,6 @@ let
   yusdacra = builtins.readFile ./yusdacra.key.pub;
   wolumonde = builtins.readFile ./wolumonde.key.pub;
 in {
-  "wgWolumondeKey.age".publicKeys = [yusdacra wolumonde];
-  "wgTkarontoKey.age".publicKeys = [yusdacra];
   "bernbotToken.age".publicKeys = [yusdacra wolumonde];
   "musikquadConfig.age".publicKeys = [yusdacra wolumonde];
   "nixGithubAccessToken.age".publicKeys = [yusdacra];
