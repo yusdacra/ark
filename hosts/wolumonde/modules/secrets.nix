6c5c7a4479a1d0f236940c511f4ed121e05ae2fd
dusk <y.bera003.06@protonmail.com>
2025-04-19 22:03:19 +0900

    feat: tailscale

--- hosts/wolumonde/modules/secrets.nix
+++ hosts/wolumonde/modules/secrets.nix
@@ -1,14 +1,15 @@
 {lib, ...}: {
   age.secrets.bernbotToken.file = ../../../secrets/bernbotToken.age;
-  age.secrets.wgWolumondeKey = {
-    file = ../../../secrets/wgWolumondeKey.age;
-    mode = "600";
-    owner = "systemd-network";
-    group = "systemd-network";
-  };
+  age.secrets.websiteConfig.file = ../../../secrets/websiteConfig.age;
+  age.secrets.pdsConfig.file = ../../../secrets/pdsConfig.age;
+  # age.secrets.wgWolumondeKey = {
+  #   file = ../../../secrets/wgWolumondeKey.age;
+  #   mode = "600";
+  #   owner = "systemd-network";
+  #   group = "systemd-network";
+  # };
   # age.secrets.musikquadConfig.file = ../../../secrets/musikquadConfig.age;
   # age.secrets.tmodloaderServerPass.file = ../../../secrets/tmodloaderServerPass.age;
-  age.secrets.websiteConfig.file = ../../../secrets/websiteConfig.age;
   # age.secrets.giteaActRunnerToken.file = ../../../secrets/giteaActRunnerToken.age;
   # age.secrets.forgejoActRunnerToken.file = ../../../secrets/forgejoActRunnerToken.age;
   # age.secrets.xrayConfig = {
@@ -18,5 +19,4 @@
   #   # owner = "xray";
   #   # group = "xray";
   # };
-  age.secrets.pdsConfig.file = ../../../secrets/pdsConfig.age;
 }
