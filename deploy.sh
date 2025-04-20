129c8eaa36e9501e4899f577a0214d87ddb113ad
dusk <y.bera003.06@protonmail.com>
2025-04-20 23:53:32 +0900

    build: make sure deploy script adds changes

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -x
 
 nix flake update
 
-git commit -m "chore: update flake dependencies (deploy) [skip ci]"; git push
+git add .; git commit -m "chore: update flake dependencies (deploy) [skip ci]"; git push
 
 nix run .#apps.nixinate.wolumonde -L --show-trace
