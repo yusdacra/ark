597e8873fedef48b103cd70bf16d88d53f425cb1
dusk <y.bera003.06@protonmail.com>
2025-04-20 23:02:19 +0900

    feat: remove unused code for arpa domain

--- hosts/wolumonde/modules/arpa.nix
+++ hosts/wolumonde/modules/arpa.nix
@@ -1,20 +1,4 @@
 {config, ...}: {
-  age.secrets.arpaCert = {
-    file = ../../../secrets/arpaCert.age;
-    mode = "600";
-    owner = "nginx";
-    group = "nginx";
-  };
-  age.secrets.arpaKey = {
-    file = ../../../secrets/arpaKey.age;
-    mode = "600";
-    owner = "nginx";
-    group = "nginx";
-  };
-
   services.nginx.virtualHosts."9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa" = {
-    # forceSSL = true;
-    # sslCertificate = config.age.secrets.arpaCert.path;
-    # sslCertificateKey = config.age.secrets.arpaKey.path;
   };
 }
