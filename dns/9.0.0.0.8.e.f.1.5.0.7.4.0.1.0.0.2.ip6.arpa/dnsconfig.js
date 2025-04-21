30d2ab3d17cea5cfa35ef7d811cf027a3ac259e8
dusk <y.bera003.06@protonmail.com>
2025-04-22 04:24:14 +0900

    feat: add dns txt info

--- dns/9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa/dnsconfig.js
+++ dns/9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa/dnsconfig.js
@@ -5,6 +5,9 @@ D("9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa", REG_CHANGEME,
 	DnsProvider(DSP_CLOUDFLARE),
 	DefaultTTL(1),
 	A("@", "23.88.101.188", CF_PROXY_ON),
-	TXT("@", "\"entity with serial id 90008 uses it/they pronouns, and prefers 3pp. please do not think of it as a human. connect to https://gaze.systems/about for more information.\""),
+	TXT("@", "an information endpoint for entity with serial id /90008/."),
+	TXT("@", "entity linked here uses it/they pronouns. it can be referred to as /dusk/ or /dawn/."),
+	TXT("@", "this one is not a human, try to refer to it in third person. use /this one/, /this thing/, etc."),
+	TXT("@", "try connecting to https://gaze.systems/about for more information."),
 );
 
