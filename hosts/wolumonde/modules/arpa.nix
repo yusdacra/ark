c0937f9c4a72a29ff24eadb25b284a85e4971524
dusk <y.bera003.06@protonmail.com>
2025-04-22 02:57:46 +0300

    feat: add some contact methods to arpa domain

--- hosts/wolumonde/modules/arpa.nix
+++ hosts/wolumonde/modules/arpa.nix
@@ -7,6 +7,10 @@ let
     of entity with serial id /90008/. you may want to open a
     connection to https://gaze.systems/about for more data.
 
+    /discord         yusdacra/
+    /bsky       @gaze.systems/
+    /email 90008@gaze.systems/
+
     /dig +short TXT 9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa/
   '';
   root = pkgs.runCommand "root" {} ''
