768b39b7567c4f01d6f17da32dfc13ad1e0817ef
dusk <y.bera003.06@protonmail.com>
2025-04-22 02:33:07 +0900

    feat: add more text to arpa domain [skip ci]

--- hosts/wolumonde/modules/arpa.nix
+++ hosts/wolumonde/modules/arpa.nix
@@ -1,7 +1,12 @@
 {pkgs, config, ...}:
 let
   index = pkgs.writeText "index.txt" ''
-    meow :3c
+    hi there~
+
+    you are currently interfacing with one of the information endpoints of entity with serial id /90008/.
+
+    you may want to open a connection to https://gaze.systems/about to read more about it.
+    alternatively run /dig +short TXT 9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa/ in your preferred terminal interface for more data.
   '';
   root = pkgs.runCommand "root" {} ''
     mkdir -p $out
