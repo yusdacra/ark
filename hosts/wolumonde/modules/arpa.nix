bc3367627be31aa5142cd3b5bfa72538c50ceba4
dusk <y.bera003.06@protonmail.com>
2025-04-22 00:34:07 +0900

    feat: add index page to arpa domain

--- hosts/wolumonde/modules/arpa.nix
+++ hosts/wolumonde/modules/arpa.nix
@@ -1,5 +1,8 @@
-{config, ...}: {
+{pkgs, config, ...}: {
   services.nginx.virtualHosts."9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa" = {
+    locations."/".index = pkgs.writeText "index.txt" ''
+      meow :3c
+    '';
     quic = true;
     kTLS = true;
   };
