57df667374ba8d5a31d194bc6d2a51ddfef723d5
dusk <y.bera003.06@protonmail.com>
2025-04-22 07:02:45 +0900

    feat: serve atproto-did for arpa domain

--- hosts/wolumonde/modules/atproto.nix
+++ hosts/wolumonde/modules/atproto.nix
@@ -5,8 +5,6 @@
     ${lib.concatStringsSep "\n" (lib.mapAttrsToList mkFileCopy files)}
   '';
   mkWellKnownCfg = files: {
-    useACMEHost = "gaze.systems";
-    forceSSL = true;
     locations."/.well-known/".extraConfig = ''
       add_header content-type text/plain;
       add_header access-control-allow-origin *;
@@ -14,15 +12,24 @@
     '';
   };
   mkDidWebCfg = domain: {
-    "${domain}" = mkWellKnownCfg {
+    "${domain}" = (mkWellKnownCfg {
       "did.json" = ../../../secrets/${domain}.did;
       "atproto-did" = pkgs.writeText "server" "did:web:${domain}";
-    };
+    }) // (lib.optionalAttrs (lib.hasSuffix "gaze.systems" domain) {
+      useACMEHost = "gaze.systems";
+      forceSSL = true;
+    });
   };
 in {
   services.nginx.virtualHosts = {
-    "gaze.systems" = mkWellKnownCfg {
+    "gaze.systems" = (mkWellKnownCfg {
       "atproto-did" = pkgs.writeText "server" "did:plc:dfl62fgb7wtjj3fcbb72naae";
+    }) // {
+      useACMEHost = "gaze.systems";
+      forceSSL = true;
+    };
+    "9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa" = mkWellKnownCfg {
+      "atproto-did" = pkgs.writeText "server" "did:plc:flbbrsgh46heoumgbemhfkvm";
     };
   } // (mkDidWebCfg "dawn.gaze.systems")
   // (mkDidWebCfg "guestbook.gaze.systems");
