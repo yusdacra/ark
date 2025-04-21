6ab876ba589af3d2bbbb93469e989c963518a2c5
dusk <y.bera003.06@protonmail.com>
2025-04-22 00:54:19 +0900

    fix: index tostring

--- hosts/wolumonde/modules/arpa.nix
+++ hosts/wolumonde/modules/arpa.nix
@@ -1,8 +1,8 @@
 {pkgs, config, ...}: {
   services.nginx.virtualHosts."9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa" = {
-    locations."/".index = pkgs.writeText "index.txt" ''
+    locations."/".index = toString (pkgs.writeText "index.txt" ''
       meow :3c
-    '';
+    '');
     quic = true;
     kTLS = true;
   };
