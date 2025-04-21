a285529983ba97671d9961d7872c2ebdc7cda7f1
dusk <y.bera003.06@protonmail.com>
2025-04-22 00:58:41 +0900

    fix: alias instead of index i think

--- hosts/wolumonde/modules/arpa.nix
+++ hosts/wolumonde/modules/arpa.nix
@@ -1,6 +1,6 @@
 {pkgs, config, ...}: {
   services.nginx.virtualHosts."9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa" = {
-    locations."/".index = toString (pkgs.writeText "index.txt" ''
+    locations."/".alias = toString (pkgs.writeText "index.txt" ''
       meow :3c
     '');
     quic = true;
