e9f8cf5a0eaef8a140badf98f5b44e2f97399461
dusk <y.bera003.06@protonmail.com>
2025-04-22 01:02:51 +0900

    fix: serve root

--- hosts/wolumonde/modules/arpa.nix
+++ hosts/wolumonde/modules/arpa.nix
@@ -1,8 +1,16 @@
-{pkgs, config, ...}: {
+{pkgs, config, ...}:
+let
+  index = pkgs.writeText "index.txt" ''
+    meow :3c
+  '';
+  root = pkgs.runCommand "root" {} ''
+    mkdir -p $out
+    ln -s ${index} $out/index.txt
+  '';
+in
+{
   services.nginx.virtualHosts."9.0.0.0.8.e.f.1.5.0.7.4.0.1.0.0.2.ip6.arpa" = {
-    locations."/".alias = toString (pkgs.writeText "index.txt" ''
-      meow :3c
-    '');
+    inherit root;
     quic = true;
     kTLS = true;
   };
