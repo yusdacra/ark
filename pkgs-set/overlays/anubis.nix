1bfc23d5aeecc90ef34a855f38ac55571ee9e632
dusk <y.bera003.06@protonmail.com>
2025-04-29 17:15:54 +0300

    chore(treewide): update anubis package

--- pkgs-set/overlays/anubis.nix
+++ pkgs-set/overlays/anubis.nix
@@ -2,38 +2,18 @@ final: prev: {
   anubis =
     let
       pname = "anubis";
-      version = "1.17.0-beta4";
+      version = "1.17.0";
 
       src = final.fetchFromGitHub {
         owner = "TecharoHQ";
         repo = "anubis";
         tag = "v${version}";
-        hash = "sha256-UP6zWaosz8t43K1AgNH0e3QOCVNfX+kka2CfgVfbPvA=";
-      };
-
-      anubisXess = final.buildNpmPackage {
-        inherit src version;
-        pname = "${pname}-xess";
-
-        npmDepsHash = "sha256-QrW0grgNRZRum2mCec86Za1UV4R5QSRlhjVYFsZDwY8=";
-
-        buildPhase = ''
-          runHook preBuild
-          npx postcss ./xess/xess.css -o xess.min.css
-          runHook postBuild
-        '';
-
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out
-          cp xess.min.css $out
-          runHook postInstall
-        '';
+        hash = "sha256-pwTb1P5gCxdiXylRo7jhykbJkJA4l3hE+imN9A2EF7g=";
       };
     in
     prev.anubis.overrideAttrs (old: {
       inherit src version;
       name = "${pname}-${version}";
-      vendorHash = "sha256-ro1Ym0RxstnzkRvxfAM5ya4KoAdw+vQggFNSyqr+dnw=";
+      vendorHash = "sha256-v9GsTUzBYfjh6/ETBbFpN5dqMzMaOz8w39Xz1omaPJE=";
     });
 }
