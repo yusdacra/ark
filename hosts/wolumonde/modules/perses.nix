a2df5c7f82615a27fe86a0dac6fa1a4dbcf093bc
dusk <y.bera003.06@protonmail.com>
2025-06-30 18:28:46 +0300

    feat(wolumonde): make perses use pocket id [skip ci]

--- hosts/wolumonde/modules/perses.nix
+++ hosts/wolumonde/modules/perses.nix
@@ -15,7 +15,13 @@ let
     security = {
       enable_auth = true;
       authentication = {
-        providers.enable_native = true;
+        providers.oidc = [{
+          slug_id = "pocketid";
+          name = "Pocket ID";
+          client_id = "aa583db6-e03c-4490-853a-7f2b3e089fbe";
+          issuer = config.services.pocket-id.settings.APP_URL;
+          scopes = ["openid profile email"];
+        }];
         disable_sign_up = true;
       };
       cookie = {
