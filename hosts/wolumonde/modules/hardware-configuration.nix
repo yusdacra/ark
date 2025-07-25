52ca14a65471886536d56d284c5adb24d0cf4a33
dusk <y.bera003.06@protonmail.com>
2025-07-25 18:22:18 +0300

    chore(wolumonde): removed hetzner volume

--- hosts/wolumonde/modules/hardware-configuration.nix
+++ hosts/wolumonde/modules/hardware-configuration.nix
@@ -12,16 +12,16 @@
     device = "/dev/sda1";
     fsType = "ext4";
   };
-  fileSystems."/mnt/data" = {
-    device = "/dev/disk/by-id/scsi-0HC_Volume_102930299";
-    fsType = "btrfs";
-    options = [ "noatime" "autodefrag" "compress-force=zstd:8" ];
-  };
-  services.beesd.filesystems = {
-    "-" = {
-      spec = "/dev/disk/by-id/scsi-0HC_Volume_102930299";
-      hashTableSizeMB = 48;
-      verbosity = "crit";
-    };
-  };
+  # fileSystems."/mnt/data" = {
+  #   device = "/dev/disk/by-id/scsi-0HC_Volume_102930299";
+  #   fsType = "btrfs";
+  #   options = [ "noatime" "autodefrag" "compress-force=zstd:8" ];
+  # };
+  # services.beesd.filesystems = {
+  #   "-" = {
+  #     spec = "/dev/disk/by-id/scsi-0HC_Volume_102930299";
+  #     hashTableSizeMB = 48;
+  #     verbosity = "crit";
+  #   };
+  # };
 }
