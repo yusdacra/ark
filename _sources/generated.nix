d19457f676c6895742161f16c3e03ef4fbe23012
dusk <y.bera003.06@protonmail.com>
2025-09-03 00:17:22 +0300

    chore(nix): update inputs [skip ci]

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -15,17 +15,17 @@
   };
   blog = {
     pname = "blog";
-    version = "ce5acd14c0477a72e93f292a44821f9a98694226";
+    version = "f412f93eda51585d57656938b9f5ebd54d3146cf";
     src = fetchgit {
       url = "https://git.gaze.systems/90008/website.git";
-      rev = "ce5acd14c0477a72e93f292a44821f9a98694226";
+      rev = "f412f93eda51585d57656938b9f5ebd54d3146cf";
       fetchSubmodules = false;
       deepClone = false;
       leaveDotGit = false;
       sparseCheckout = [ ];
-      sha256 = "sha256-kNxZwbqaSotYeDVgqt4eB7o/UYlP2kL5qK35rIC/+yA=";
+      sha256 = "sha256-wef6nkosHqD/q7dK52pHJqzELT3J0+ZwO9NcOu0gN2M=";
     };
-    date = "2025-09-01";
+    date = "2025-09-03";
   };
   gomod2nix = {
     pname = "gomod2nix";
