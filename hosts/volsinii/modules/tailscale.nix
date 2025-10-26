5751a0392ad3036f97d54030213a5c869e7a1cd0
dusk <y.bera003.06@protonmail.com>
2025-10-26 05:25:35 +0300

    add tailscale to volsinii

---
+++ hosts/volsinii/modules/tailscale.nix
@@ -0,0 +1,6 @@
+{config, ...}: {
+  imports = [../../../modules/network/tailscale.nix];
+  
+  # age.secrets.tailscaleAuthKey.file = ../../../secrets/tailscaleAuthKey.age;
+  # services.tailscale.authKeyFile = config.age.secrets.tailscaleAuthKey.path;
+}
