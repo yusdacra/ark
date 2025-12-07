92f632c17243cd56c624f05ff55813540f6a0363
dawn <90008@gaze.systems>
2025-12-08 00:22:34 +0300

    add ipv6 records on dns

--- dns/dnsconfig.js
+++ dns/dnsconfig.js
@@ -1,33 +1,61 @@
 var DSP_CLOUDFLARE = NewDnsProvider("cloudflare");
 var REG_NONE = NewRegistrar("none");
 
-var WOLUMONDE_IP = "23.88.101.188";
-var DZWONEK_IP = "94.237.26.47";
-var TRIMOUNTS_IP = "159.195.58.28";
+var DZWONEK_IP4 = "94.237.26.47";
+var DZWONEK_IP6 = "2a04:3542:1000:910:6898:1dff:fea1:4b4b";
+var DZWONEK_IPS = [DZWONEK_IP4, DZWONEK_IP6];
+var TRIMOUNTS_IP4 = "159.195.58.28";
+var TRIMOUNTS_IP6 = "2a0a:4cc0:c1:e83d::b00b";
+var TRIMOUNTS_IPS = [TRIMOUNTS_IP4, TRIMOUNTS_IP6];
+
+function host(name, ips, opts) {
+    return [
+        A(name, ips[0], opts),
+        AAAA(name, ips[1], opts),
+    ];
+}
+
+function hosts(_names, ips, opts) {
+    var names = [];
+    if (typeof _names === "string")
+        names.push(_names);
+    else
+        names = _names;
+
+    var records = [];
+    _.each(names, function (name) {
+        _.each(host(name, ips, opts), function (r) {
+            records.push(r);
+        });
+    });
+    return records;
+}
+
+function TRIMOUNTS(names, opts) {
+    return hosts(names, TRIMOUNTS_IPS, opts);
+}
+function DZWONEK(names, opts) {
+    return hosts(names, DZWONEK_IPS, opts);
+}
+
+function IGNORE_ACME() {
+    return IGNORE_NAME("_acme-challenge");
+}
 
 D(
     "gaze.systems",
     REG_NONE,
     DnsProvider(DSP_CLOUDFLARE),
     DefaultTTL(1),
-    A("@", TRIMOUNTS_IP, CF_PROXY_OFF, TTL(60)),
-    A("doc", TRIMOUNTS_IP, CF_PROXY_OFF),
-    A("pmart", TRIMOUNTS_IP, CF_PROXY_OFF),
-    // A("webhook", WOLUMONDE_IP, CF_PROXY_OFF),
-    A("dash", TRIMOUNTS_IP, CF_PROXY_OFF), // perses
-    A("knot", TRIMOUNTS_IP, CF_PROXY_OFF, TTL(60)),
-    A("spindle", TRIMOUNTS_IP, CF_PROXY_OFF, TTL(60)),
-    A("id", TRIMOUNTS_IP, CF_PROXY_OFF),
-    // atp
-    A("guestbook", TRIMOUNTS_IP, CF_PROXY_OFF),
-    // dzwonek
-    A("vpn", DZWONEK_IP, CF_PROXY_OFF),
-    // A("meow", WOLUMONDE_IP, CF_PROXY_OFF),
-    // thing
-    // TXT("id", "a data endpoint for entity with serial id /90008/."),
-    // TXT("id", "entity linked here uses it/they pronouns. it can be referred to as /dusk/ or /dawn/."),
-    // TXT("id", "this one is not a human, try to refer to it in third person. use /this one/, /this thing/, etc."),
-    // TXT("id", "try connecting to https://gaze.systems/about for more information."),
+    TRIMOUNTS(
+        [
+            "@", "doc", "pmart", "dash", "id",
+            "knot", "spindle",
+            "guestbook",
+        ],
+        CF_PROXY_OFF,
+    ),
+    DZWONEK("vpn", CF_PROXY_OFF),
     // github pages
     CNAME("dev", "90-008.github.io."),
     // fastmail
@@ -37,7 +65,7 @@ D(
     MX("@", 10, "in1-smtp.messagingengine.com."),
     MX("@", 20, "in2-smtp.messagingengine.com."),
     TXT("@", "v=spf1 include:spf.messagingengine.com ?all"),
-    TXT("_dmarc", "v=DMARC1; p=none;"),
+    TXT("_dmarc", "v=DMARC1; p=reject;"),
     // resend
     MX("send.poke", 10, "feedback-smtp.us-east-1.amazonses.com."),
     TXT(
@@ -48,6 +76,7 @@ D(
     // atproto
     TXT("_atproto.eris", "did=did:plc:bxjnsrfzozl365rsdo5yvuz5", TTL(60)),
     TXT("_atproto.drew", "did=did:plc:vo6ie3kd6xvpjlof4pnb2zzp", TTL(60)),
+    IGNORE_ACME(),
 );
 
 D(
@@ -55,7 +84,7 @@ D(
     REG_NONE,
     DnsProvider(DSP_CLOUDFLARE),
     DefaultTTL(1),
-    A("@", TRIMOUNTS_IP, CF_PROXY_ON),
+    TRIMOUNTS("@", CF_PROXY_ON),
     TXT("@", "a data endpoint for entity with serial id /90008/."),
     TXT(
         "@",
@@ -69,9 +98,7 @@ D(
         "@",
         "try connecting to https://gaze.systems/about for more information.",
     ),
-    // atproto
-    // TXT("_atproto", "did=did:plc:dfl62fgb7wtjj3fcbb72naae"),
-    IGNORE_NAME("_acme-challenge"),
+    IGNORE_ACME(),
 );
 
 D(
@@ -79,11 +106,11 @@ D(
     REG_NONE,
     DnsProvider(DSP_CLOUDFLARE),
     DefaultTTL(1),
-    A("@", TRIMOUNTS_IP, CF_PROXY_OFF),
+    TRIMOUNTS("@", CF_PROXY_OFF),
     TXT("@", "v=spf1 -all"),
     TXT("_dmarc", "v=DMARC1; p=reject;"),
     TXT("_atproto", "did=did:plc:dfl62fgb7wtjj3fcbb72naae", TTL(60)),
-    IGNORE_NAME("_acme-challenge"),
+    IGNORE_ACME(),
 );
 
 var EMAIL_TTL = 86400;
@@ -93,9 +120,8 @@ D(
     REG_NONE,
     DnsProvider(DSP_CLOUDFLARE),
     DefaultTTL(1),
-    A("@", TRIMOUNTS_IP, CF_PROXY_OFF),
-    A("nucleus", DZWONEK_IP, CF_PROXY_OFF),
-    A("trill", DZWONEK_IP, CF_PROXY_OFF),
+    TRIMOUNTS("@", CF_PROXY_OFF),
+    DZWONEK(["nucleus", "trill"], CF_PROXY_OFF),
     // atproto
     TXT("_atproto", "did=did:plc:dfl62fgb7wtjj3fcbb72naae"),
     TXT("_atproto.nil", "did=did:plc:dumbmutt4po52ept2tczimje"),
@@ -152,12 +178,11 @@ D(
     ),
 
     // mta-sts
-    A("mta-sts", TRIMOUNTS_IP, CF_PROXY_OFF),
+    TRIMOUNTS("mta-sts", CF_PROXY_OFF),
     TXT("_mta-sts", "v=STSv1; id=20250930T1945", TTL(EMAIL_TTL)),
 
     // autoconfig
-    A("autoconfig", TRIMOUNTS_IP, CF_PROXY_OFF),
-    A("autodiscover", TRIMOUNTS_IP, CF_PROXY_OFF),
+    TRIMOUNTS(["autoconfig", "autodiscover"], CF_PROXY_OFF),
 
     // autodiscovery
     SRV(
@@ -172,5 +197,5 @@ D(
     SRV("_imaps._tcp", 0, 1, 993, "imap.migadu.com.", TTL(EMAIL_TTL)),
     SRV("_pop3s._tcp", 0, 1, 995, "pop.migadu.com.", TTL(EMAIL_TTL)),
 
-    IGNORE_NAME("_acme-challenge"),
+    IGNORE_ACME(),
 );
